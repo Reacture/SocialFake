106638530c7f355a05c608697035413452dcc589
Gyuwon <gyuwon@live.com>
2017-08-31 15:44:37 +0900

    UserCreated 도메인 이벤트를 추가합니다.

---
+++ Identity/SocialFake.Identity.Contract/Identity/Events/UserCreated.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Khala.EventSourcing;
+using Khala.EventSourcing.Sql;
+using Newtonsoft.Json;
+
+namespace SocialFake.Identity.Events
+{
+    public class UserCreated : DomainEvent, IUniqueIndexedDomainEvent
+    {
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<string, string> UniqueIndexedProperties
+            => new Dictionary<string, string>
+            {
+                [nameof(Username)] = Username
+            };
+    }
+}
