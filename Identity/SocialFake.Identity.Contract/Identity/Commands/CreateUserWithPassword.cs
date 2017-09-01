91d56054f8a00b78799a2f40a92cbcb5a0047a85
Gyuwon <gyuwon@live.com>
2017-09-01 16:51:44 +0900

    CreateUserWithPassword 명령을 추가합니다.

---
+++ Identity/SocialFake.Identity.Contract/Identity/Commands/CreateUserWithPassword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Khala.Messaging;
+
+namespace SocialFake.Identity.Commands
+{
+    public class CreateUserWithPassword : IPartitioned
+    {
+        public Guid UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string PartitionKey => UserId.ToString();
+    }
+}
