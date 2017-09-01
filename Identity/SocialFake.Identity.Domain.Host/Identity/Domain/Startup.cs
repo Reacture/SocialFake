d3cbeb1a12f479f0ff01db567fa181af92fb82b2
Gyuwon <gyuwon@live.com>
2017-09-01 15:20:55 +0900

    SocialFake.Identity.Domain.Host 프로젝트를 추가합니다.

---
+++ Identity/SocialFake.Identity.Domain.Host/Identity/Domain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using SocialFake.Identity.Domain;
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(Startup))]
+
+namespace SocialFake.Identity.Domain
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
