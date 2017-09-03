80813d06e39a3316cf6a9b0210b96ba9228c93bd
Gyuwon <gyuwon@live.com>
2017-09-03 19:22:11 +0900

    ChangeDisplayNames 명령을 추가합니다.

---
+++ Identity/SocialFake.Identity.Contract/Identity/Commands/ChangeDisplayNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Khala.Messaging;
+using Newtonsoft.Json;
+
+namespace SocialFake.Identity.Commands
+{
+    public class ChangeDisplayNames : IPartitioned
+    {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        [JsonIgnore]
+        public string PartitionKey => UserId.ToString();
+    }
+}
