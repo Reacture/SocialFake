a78c92fe91379beb03159c5021d99fe8b0b127bd
Gyuwon <gyuwon@live.com>
2017-09-03 22:57:29 +0900

    읽기 모델 호스트 프로젝트를 추가합니다.

---
+++ Facade/SocialFake.Facade.Host/Facade/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Khala.Messaging;
+using Khala.Messaging.Azure;
+using Microsoft.Owin;
+using Microsoft.ServiceBus.Messaging;
+using Owin;
+using SocialFake.Facade;
+using SocialFake.Facade.ReadModel;
+using Swashbuckle.Application;
+using Swashbuckle.Swagger;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SocialFake.Facade
+{
+    public class Startup
+    {
+        public static string AppName { get; } = typeof(Startup).Assembly.GetName().Name;
+
+        public static string InstanceName { get; } = $"{AppName}-{Guid.NewGuid().ToString("n")}";
+
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            IMessageSerializer messageSerializer = new JsonMessageSerializer();
+            EventDataSerializer eventSerializer = new EventDataSerializer(messageSerializer);
+
+            var eventHubClient = EventHubClient.CreateFromConnectionString(
+                ConfigurationManager.ConnectionStrings["MessagingNamespace"].ConnectionString,
+                ConfigurationManager.AppSettings["MessageStream"]);
+
+            var eventHandlerHost = new EventProcessorHost(
+                InstanceName,
+                ConfigurationManager.AppSettings["MessageStream"],
+                ConfigurationManager.AppSettings["FacadeConsumerGroup"],
+                ConfigurationManager.ConnectionStrings["MessagingNamespace"].ConnectionString,
+                ConfigurationManager.ConnectionStrings["MessagingStorage"].ConnectionString);
+
+            IMessageHandler messageHandler = new ReadModelGenerator(() => new SocialFakeDbContext());
+
+            app.UseEventMessageProcessor(
+                eventHandlerHost,
+                eventSerializer,
+                messageHandler);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SocialFakeDbContext>();
+            IContainer container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            config.MapHttpAttributeRoutes();
+            config.EnableSwagger(ConfigureSwagger).EnableSwaggerUi();
+
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureSwagger(SwaggerDocsConfig config)
+        {
+            Schema GuidSchemaFactory()
+            {
+                return new Schema
+                {
+                    type = "string",
+                    format = "uuid",
+                    example = Guid.NewGuid().ToString()
+                };
+            }
+
+            config.MapType<Guid>(GuidSchemaFactory);
+            config.SingleApiVersion("v1", "Facade API v1");
+        }
+    }
+}
---
+++ Facade/SocialFake.Facade.Host/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SocialFake
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IHttpActionResult Index()
+        {
+            return Redirect(new Uri("/swagger/ui/index", UriKind.Relative));
+        }
+    }
+}
