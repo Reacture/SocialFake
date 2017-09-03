b711fa90fd1ece2014f7540710160b2f65258023
Gyuwon <gyuwon@live.com>
2017-09-03 23:44:17 +0900

    UserDto 클래스를 추가합니다.

---
+++ SocialFake.Facade.Contract/Facade/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SocialFake.Facade
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
